ca479de371b8ab97ee1c966f3ac9f6e049750335
JSantos92 <skyjgod@gmail.com>
2017-04-07 03:10:52 +0100

    First working prototype

--- MyRibbon.cs
+++ MyRibbon.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
 using QRCoder;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using Office = Microsoft.Office.Core;
 using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Excel = Microsoft.Office.Interop.Excel;
 
 // TODO:  Follow these steps to enable the Ribbon (XML) item:
 
@@ -95,59 +97,133 @@ namespace MyRibbonAddIn
         {
             Newtonsoft.Json.Linq.JObject responses = this.addin.getFormResponses(globalUrl);
 
-            List<string> questions = parseQuestions(responses);
+            List<Question> questions = parseResponses(responses);
 
-            List<string> answers = parseAnswers(responses);
-
-            PowerPoint.CustomLayout layout = this.addin.Application.ActivePresentation.SlideMaster.CustomLayouts[1];
+            PowerPoint.CustomLayout layout = this.addin.Application.ActivePresentation.SlideMaster.CustomLayouts[4];
 
             for (int i = 0; i < questions.Count(); i++)
             {
-                this.addin.Application.ActivePresentation.Slides.AddSlide(this.addin.currentSlide+1, layout);
+                //Adicionar novo slide e mudar slide de foco
 
+                this.addin.Application.ActivePresentation.Slides.AddSlide(this.addin.currentSlide + 1, layout);
                 this.addin.Application.ActiveWindow.Presentation.Slides[this.addin.currentSlide + 1].Select();
 
-                PowerPoint.Shape textBox = this.addin.Application.ActiveWindow.View.Slide.Shapes.AddTextbox(
-                Office.MsoTextOrientation.msoTextOrientationHorizontal, 305, 50, 400, 400);
-                textBox.TextFrame.TextRange.Font.Size = 48;
-                textBox.TextFrame.TextRange.InsertAfter(questions[i]);
+                //Output do Titulo da Pergunta 
+                this.addin.Application.ActiveWindow.View.Slide.Shapes.Title.TextFrame.TextRange.Text = questions[i].Title;
 
-                // TODO remove placeholders
+/*              //Output das opçoes de resposta
+                this.addin.Application.ActiveWindow.View.Slide.Shapes.Placeholders().Delete(2);
+*/
+                //Output da opção de resposta 
+                this.addin.Application.ActiveWindow.View.Slide.Shapes.Placeholders(2).TextFrame.TextRange.Text = " ";
+
+                //Output do Gráfico 
+                this.addin.Application.ActiveWindow.View.Slide.Shapes.AddChart(Office.XlChartType.xl3DColumn, 200F, 70F, 600F, 500F);
+
+                //Access the added chart
+                PowerPoint.Chart ppChart = this.addin.Application.ActiveWindow.View.Slide.Shapes[3].Chart;
+
+                //Access the chart data
+                PowerPoint.ChartData chartData = ppChart.ChartData;
+
+                //Create instance to Excel workbook to work with chart data
+                Excel.Workbook dataWorkbook = (Excel.Workbook)chartData.Workbook;
+
+                //Accessing the data worksheet for chart
+                Excel.Worksheet dataSheet = dataWorkbook.Worksheets[1];
+
+                //Setting the range of chart
+                Excel.Range tRange = dataSheet.Cells.get_Range("A1", "B5");
+
+                //Applying the set range on chart data table
+                Excel.ListObject tbl1 = dataSheet.ListObjects["Tabela1"];
+                tbl1.Resize(tRange);
+
+                //Setting values for categories and respective series data TODO : for loop para as choices
+
+                ((Excel.Range)(dataSheet.Cells.get_Range("A2"))).FormulaR1C1 = questions[i].Choices[0].option;
+                ((Excel.Range)(dataSheet.Cells.get_Range("A3"))).FormulaR1C1 = questions[i].Choices[1].option;
+                ((Excel.Range)(dataSheet.Cells.get_Range("A4"))).FormulaR1C1 = questions[i].Choices[2].option;
+                ((Excel.Range)(dataSheet.Cells.get_Range("A5"))).FormulaR1C1 = questions[i].Choices[3].option;
+                ((Excel.Range)(dataSheet.Cells.get_Range("B2"))).FormulaR1C1 = questions[i].Choices[1].count.ToString();
+                ((Excel.Range)(dataSheet.Cells.get_Range("B3"))).FormulaR1C1 = questions[i].Choices[1].count.ToString();
+                ((Excel.Range)(dataSheet.Cells.get_Range("B4"))).FormulaR1C1 = questions[i].Choices[2].count.ToString(); 
+                ((Excel.Range)(dataSheet.Cells.get_Range("B5"))).FormulaR1C1 = questions[i].Choices[3].count.ToString(); 
+
+                //Setting chart title
+               // ppChart.ChartTitle.Font.Italic = true;
+                ppChart.ChartTitle.Text = " ";
+                /*ppChart.ChartTitle.Font.Size = 18;
+                ppChart.ChartTitle.Font.Color = Color.Black.ToArgb();
+                ppChart.ChartTitle.Format.Line.Visible = Office.MsoTriState.msoTrue;
+                ppChart.ChartTitle.Format.Line.ForeColor.RGB = Color.Black.ToArgb();*/
+
+                //Accessing Chart value axis
+                PowerPoint.Axis valaxis = ppChart.Axes(PowerPoint.XlAxisType.xlValue, PowerPoint.XlAxisGroup.xlPrimary);
+
+                //Setting values axis units
+                valaxis.MajorUnit = 100.0F;
+                valaxis.MinorUnit = 50.0F;
+                valaxis.MinimumScale = 0.0F;
+                valaxis.MaximumScale = 500.0F;
+
+                //Accessing Chart Depth axis
+                PowerPoint.Axis Depthaxis = ppChart.Axes(PowerPoint.XlAxisType.xlSeriesAxis, PowerPoint.XlAxisGroup.xlPrimary);
+                Depthaxis.Delete();
+
+                //Setting chart rotation
+                ppChart.Rotation = 0; //Y-Value
+                ppChart.Elevation = 0; //X-Value
+                ppChart.RightAngleAxes = false; 
+
+    
+                
+                // Cálcular numero de votos por opção
+
+
+
+                //Output do gráfico das respostas
+
+
+
+
             }
+        
 
 
 
         }
 
-        public List<string> parseQuestions(Newtonsoft.Json.Linq.JObject responses)
+        public List<Question> parseResponses(Newtonsoft.Json.Linq.JObject responses)
         {
-            List<string> listQuestions = new List<string>();
+            List<Question> listQuestions = new List<Question>();
 
             foreach (KeyValuePair<string, Newtonsoft.Json.Linq.JToken> response in responses)
             {
-                for (int j = 0; j < response.Value["Question"].Count(); j++) {
-                    listQuestions.Add((string)response.Value["Question"][j]);
+                Question questao = new Question();
+                questao.Title = (string)response.Value["Title"];
+
+                var responseArray = (JArray)response.Value["Answers"];
+                foreach(var r in responseArray)
+                {
+                    questao.Answers.Add(r.ToObject<string>());
                 }
+                //questao.Answers = (JArray)response.Value["Answers"].ToArray<string>();
+                questao.Type = (string)response.Value["Type"];
+
+                var choices = (JObject)response.Value["Choices"];
+                foreach (JProperty prop in choices.Properties())
+                {
+                    Choice c = new Choice(prop.Name, prop.Value.ToObject<int>());
+                    questao.Choices.Add(c);
+                }
+
+                listQuestions.Add(questao);
             }
 
             return listQuestions;
         }
-
-        public List<string> parseAnswers(Newtonsoft.Json.Linq.JObject responses)
-        {
-            List<string> listAnswers = new List<string>();
-
-            foreach (KeyValuePair<string, Newtonsoft.Json.Linq.JToken> response in responses)
-            {
-                for (int j = 0; j < response.Value["Answer"].Count(); j++)
-                {
-                    listAnswers.Add((string)response.Value["Answer"][j]);
-                }
-            }
-
-            return listAnswers;
-        }
-
+    
         #endregion
 
         #region Helpers
---
+++ Question.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRibbonAddIn
+{
+    public class Question
+    {
+        public string Title { get; set; }
+        public List<string> Answers { get; set; }
+        public string Type { get; set; }
+        public List<Choice> Choices { get; set; }
+
+        public Question()
+        {
+            this.Choices = new List<Choice>();
+            this.Answers = new List<string>();
+        }
+    }
+
+    public class Choice
+    {
+        public string option { get; set; }
+        public int count { get; set; }
+        
+        public Choice(string option, int count)
+        {
+            this.option = option;
+            this.count = count;
+        }
+    }
+
+}
